7d8e5fc391dd8697997d5d851a35ffe13b857ba0
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2025-09-12 09:20:50 -0300

    Refatoração geral: controladores, views e validações
    - Adicionados `[Authorize]` em controladores para restringir acesso.
    - Refatorado `AgendaController` com melhorias no construtor,
      validações, tratamento de exceções e remoção de redundâncias.
    - Ajustadas views para exibir nomes em vez de IDs e melhorar
      usabilidade, consistência e formatação do código.
    - Alterado campo `DataNascimento` para tipo `date` em formulário.
    - Adicionados `[Bind]` em métodos `Create` e `Edit` para proteção
      contra overposting.
    - Melhorias gerais na interface e organização do código.

--- AgendaMedicas/Controllers/EspecialidadesController.cs
+++ AgendaMedicas/Controllers/EspecialidadesController.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using AgendaMedicas.Data;
+﻿using AgendaMedicas.Data;
 using AgendaMedicas.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AgendaMedicas.Controllers
 {
+    [Authorize]
     public class EspecialidadesController : Controller
     {
         private readonly ApplicationDbContext _context;
